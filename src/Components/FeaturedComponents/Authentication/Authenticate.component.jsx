0404906c16b35b313bf6ed5e3b99a17fb6b26eb9
Viekesh <vikeshgaikwad1@gmail.com>
2024-06-06 11:48:07 +0530

    Continue To Code

--- src/Components/FeaturedComponents/Authentication/Authenticate.component.jsx
+++ src/Components/FeaturedComponents/Authentication/Authenticate.component.jsx
@@ -3,10 +3,42 @@ import "./Auth.style.scss";
 import GoogleSignInButton from "./GAuth";
 import GAuthMob from "./GAuthMob";
 import TopNav from "../Navigation/TopNav.component";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { useState } from "react";
 
 
 
 const Authenticate = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const eyeVisi = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+
+    const handleFormData = (event) => {
+        console.log(event.target.value);
+
+        setFormData((previousState) => ({
+            ...previousState,
+            [event.target.id]: event.target.value,
+        }));
+    };
+
+    const submitFormData = async (event) => {
+        event.preventDefault();
+
+        setLoading(true);
+    }
 
     return (
         <>
@@ -15,9 +47,43 @@ const Authenticate = () => {
 
                 <div className="sign_in">
                     <form>
-                        <input type="text" />
-                        <input type="text" />
-                        <button>Submit</button>
+                        <div className="form_elements y_axis_center">
+                            <input
+                                type="text"
+                                id="firstName"
+                                value={email}
+                                name="firstName"
+
+                                className="input_field"
+                                placeholder="Enter Firstname"
+                                onChange={handleFormData}
+                            />
+                        </div>
+
+                        <div className="form_elements y_axis_center">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                value={password}
+                                name="password"
+
+                                className="input_field"
+                                placeholder="Enter Password"
+                                onChange={handleFormData}
+                            />
+
+                            {
+                                showPassword ?
+                                    <AiFillEye
+                                        className="eye"
+                                        onClick={eyeVisi}
+                                    /> :
+                                    <AiFillEyeInvisible
+                                        className="eye"
+                                        onClick={eyeVisi}
+                                    />
+                            }
+                        </div>
                     </form>
                 </div>
 
